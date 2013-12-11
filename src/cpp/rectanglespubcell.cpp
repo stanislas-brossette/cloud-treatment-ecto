7d2ef4f8cfe97505c41c69dfb785ec19017e1874
Stanislas Brossette <stanislas.brossette@gmail.com>
2013-12-11 18:32:22 -0500

    Add a generic function to fill the geometry_msgs

--- src/cpp/rectanglespubcell.cpp
+++ src/cpp/rectanglespubcell.cpp
@@ -11,6 +11,7 @@ namespace cloud_treatment
 {
 	struct RectanglesPubCell
 	{
+
 		static void declare_params(tendrils& params)
 		{
 		}
@@ -21,23 +22,23 @@ namespace cloud_treatment
                                      "rectangles", "rectangles");
 			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped const> > (
                                      "rectanglemsg0", "rectanglemsg0");
-			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped > > (
+			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped const> > (
                                      "rectanglemsg1", "rectanglemsg1");
-			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped > > (
+			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped const> > (
                                      "rectanglemsg2", "rectanglemsg2");
-			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped > > (
+			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped const> > (
                                      "rectanglemsg3", "rectanglemsg3");
-			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped > > (
+			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped const> > (
                                      "rectanglemsg4", "rectanglemsg4");
-			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped > > (
+			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped const> > (
                                      "rectanglemsg5", "rectanglemsg5");
-			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped > > (
+			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped const> > (
                                      "rectanglemsg6", "rectanglemsg6");
-			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped > > (
+			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped const> > (
                                      "rectanglemsg7", "rectanglemsg7");
-			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped > > (
+			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped const> > (
                                      "rectanglemsg8", "rectanglemsg8");
-			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped > > (
+			outputs.declare<boost::shared_ptr<geometry_msgs::PolygonStamped const> > (
                                      "rectanglemsg9", "rectanglemsg9");
 		}
 
@@ -56,47 +57,39 @@ namespace cloud_treatment
 			rectanglemsg9_ = outputs["rectanglemsg9"];
 		}
 
+     boost::shared_ptr<geometry_msgs::PolygonStamped const> fillPolygonStamped(
+                           const std::vector<Eigen::Vector3f>& rectangle)
+    {
+      geometry_msgs::PolygonStamped Polygon;
+      Polygon.header.frame_id = "/world";
+      geometry_msgs::Point32 point;
+      for(std::size_t i = 0; i < rectangle.size()+1; ++i)
+      {
+        point.x = rectangle[i % rectangle.size()].x();
+        point.y = rectangle[i % rectangle.size()].y();
+        point.z = rectangle[i % rectangle.size()].z();
+        Polygon.polygon.points.push_back(point);
+      }
+      return boost::make_shared<geometry_msgs::PolygonStamped const>(Polygon);
+    }
+
 		int process(const tendrils& inputs, const tendrils& outputs)
 		{
-      geometry_msgs::PolygonStamped polygon;
-      std::cout << "rectangle test " << rectangles_->at(0)[0].x() << std::endl; 
-      polygon.header.frame_id = "/world";
-      geometry_msgs::Point32 point0;
-      geometry_msgs::Point32 point1;
-      geometry_msgs::Point32 point2;
-      geometry_msgs::Point32 point3;
-      point0.x = 0.0;
-      point0.y = 0.0;
-      point0.z = 0.0;
-      point1.x = 1.0;
-      point1.y = 0.0;
-      point1.z = 0.0;
-      point2.x = 1.0;
-      point2.y = 1.0;
-      point2.z = 0.0;
-      point3.x = 0.0;
-      point3.y = 1.0;
-      point3.z = 0.0;
-      polygon.polygon.points.push_back(point0);
-      polygon.polygon.points.push_back(point1);
-      polygon.polygon.points.push_back(point2);
-      polygon.polygon.points.push_back(point3);
-      polygon.polygon.points.push_back(point0);
-      *rectanglemsg0_ = boost::make_shared<geometry_msgs::PolygonStamped const>(polygon);
+      *rectanglemsg0_ = fillPolygonStamped(rectangles_->at(0));      
       std::cout << (*rectanglemsg0_)->polygon.points.size();
 			return ecto::OK;
 		}
     ecto::spore< std::vector< std::vector<Eigen::Vector3f> > > rectangles_;
     ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped const> > rectanglemsg0_;
-    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped > > rectanglemsg1_;
-    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped > > rectanglemsg2_;
-    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped > > rectanglemsg3_;
-    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped > > rectanglemsg4_;
-    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped > > rectanglemsg5_;
-    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped > > rectanglemsg6_;
-    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped > > rectanglemsg7_;
-    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped > > rectanglemsg8_;
-    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped > > rectanglemsg9_;
+    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped const> > rectanglemsg1_;
+    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped const> > rectanglemsg2_;
+    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped const> > rectanglemsg3_;
+    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped const> > rectanglemsg4_;
+    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped const> > rectanglemsg5_;
+    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped const> > rectanglemsg6_;
+    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped const> > rectanglemsg7_;
+    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped const> > rectanglemsg8_;
+    ecto::spore< boost::shared_ptr<geometry_msgs::PolygonStamped const> > rectanglemsg9_;
 	};
 }
 
