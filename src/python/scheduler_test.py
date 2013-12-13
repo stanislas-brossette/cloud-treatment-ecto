d4355c4c1eee98b9a64e56f2742b5837df3f53ce
Stanislas Brossette <stanislas.brossette@gmail.com>
2013-12-12 23:48:41 -0500

    Cleaned up the ecto file

--- src/python/scheduler_test.py
+++ src/python/scheduler_test.py
@@ -3,6 +3,7 @@
 import ecto, ecto_pcl, ecto_ros, ecto_pcl_ros
 import ecto_ros.ecto_sensor_msgs as ecto_sensor_msgs
 import ecto_ros.ecto_geometry_msgs as ecto_geometry_msgs
+import ecto_ros.ecto_std_msgs as ecto_std_msgs
 from ecto.opts import scheduler_options, run_plasm
 import sys
 import time
@@ -24,25 +25,19 @@ options = parser.parse_args()
 pcdfilename = options.pcdfile
 plasm = ecto.Plasm()
 
-reader = cloud_treatment.PCDReaderCell(	"Reader_ecto",
-					filename=pcdfilename)
-
+### ROS SUBSCRIBERS ###
 cloud_sub = ecto_sensor_msgs.Subscriber_PointCloud2("cloud_sub",
-    topic_name='/worldmodel_main/pointcloud_vis')
-msg2cloud = ecto_pcl_ros.Message2PointCloud("msg2cloud", format=ecto_pcl.XYZRGB)
-cloud2msg_main = ecto_pcl_ros.PointCloud2Message("cloud2msg_main")
-cloud2msg_seg = ecto_pcl_ros.PointCloud2Message("cloud2msg_seg")
+                topic_name='/worldmodel_main/pointcloud_vis')
+
+### ROS PUBLISHERS ###
 cloud_pub_main = ecto_sensor_msgs.Publisher_PointCloud2(
-                   "cloud_pub_main",topic_name='/ecto/main_cloud')
+                "cloud_pub_main",topic_name='/ecto/main_cloud')
 cloud_pub_seg = ecto_sensor_msgs.Publisher_PointCloud2(
-                   "cloud_pub_seg",topic_name='/ecto/seg_cloud')
-
-recalibrate2msg = cloud_treatment.RecalibrateMsgCell("recalMsgEnv",
-                                                     choice_index=2)
+                "cloud_pub_seg",topic_name='/ecto/seg_cloud')
 poseStamped_pub = ecto_geometry_msgs.Publisher_PoseStamped("poseStamped_pub",
-                                             topic_name="/ecto/poseStamped")
-
-rectangle2msg = cloud_treatment.RectanglesPubCell("rectangle2msg")
+                topic_name="/ecto/poseStamped")
+surfName_pub = ecto_std_msgs.Publisher_String("surfName_pub",
+                topic_name="/ecto/surfName")
 rectpub0=ecto_geometry_msgs.Publisher_PolygonStamped("rectpub0",topic_name='/ecto/polygon0')
 rectpub1=ecto_geometry_msgs.Publisher_PolygonStamped("rectpub1",topic_name='/ecto/polygon1')
 rectpub2=ecto_geometry_msgs.Publisher_PolygonStamped("rectpub2",topic_name='/ecto/polygon2')
@@ -50,15 +45,23 @@ rectpub3=ecto_geometry_msgs.Publisher_PolygonStamped("rectpub3",topic_name='/ect
 rectpub4=ecto_geometry_msgs.Publisher_PolygonStamped("rectpub4",topic_name='/ecto/polygon4')
 rectpub5=ecto_geometry_msgs.Publisher_PolygonStamped("rectpub5",topic_name='/ecto/polygon5')
 
-passthrough3d = cloud_treatment.PassThrough3DCell(
-					"passthrough3D",
-          x_min=-1.0,
-          x_max=2.0,
-          y_min=-1.9,
-          y_max=-0.5,
-          z_min=-1.0,
-          z_max=3.0)
+### ROS MSG EMITORS ###
+msg2cloud = ecto_pcl_ros.Message2PointCloud("msg2cloud", format=ecto_pcl.XYZRGB)
+cloud2msg_main = ecto_pcl_ros.PointCloud2Message("cloud2msg_main")
+cloud2msg_seg = ecto_pcl_ros.PointCloud2Message("cloud2msg_seg")
+recalibrate2msg = cloud_treatment.RecalibrateMsgCell("recalMsgEnv", choice_index=2)
+rectangle2msg = cloud_treatment.RectanglesPubCell("rectangle2msg")
+extract_header = cloud_treatment.ExtractHeaderCell('extractHeader')
 
+### CLOUD TREATMENTS ###
+passthrough3d = cloud_treatment.PassThrough3DCell(
+                "passthrough3D",
+                x_min=-1.0,
+                x_max=2.0,
+                y_min=-1.9,
+                y_max=-0.5,
+                z_min=-1.0,
+                z_max=3.0)
 stepsegmenter = cloud_treatment.StepSegmentationCell("Step_Seg",
                 z_step_1=-0.06,
                 z_step_2=0.23,
@@ -70,54 +73,62 @@ stepsegmenter = cloud_treatment.StepSegmentationCell("Step_Seg",
                 positive_threshold=0.01,
                 negative_threshold=0.0
                 )
-
 principalcomponent = cloud_treatment.PrincipalComponentExtractionCell(
                 "Principal_component",
                 length_rectangles = 0.8128,
                 width_rectangles = 0.127
                 )
-
-
 colorize = ecto_pcl.ColorizeClusters("colorize")
-
 viewer = cloud_treatment.CloudViewerCell("Viewer_ecto",
-					window_name="PCD Viewer")
+                window_name="PCD Viewer")
 
-graph = [cloud_sub["output"] >> msg2cloud[:],
-         msg2cloud[:] >> passthrough3d["input"],
+### GRAPH ###
+## SUBSCRIPTION 
+graph =  [cloud_sub["output"] >> msg2cloud[:],
+         cloud_sub["output"] >> extract_header[:]
+         ]
+## CLOUD TREATMENT
+graph += [msg2cloud[:] >> passthrough3d["input"],
          passthrough3d["output"] >> stepsegmenter["input"],
-         #principalcomponent["centroids"] >> viewer["VIPoints"],
-         #principalcomponent["rectangles"] >> viewer["rectangles"],
          stepsegmenter["clusters"] >> colorize["clusters"],
          passthrough3d["output"] >> colorize["input"], 
          stepsegmenter["clusters"] >> principalcomponent["clusters"],
-         passthrough3d["output"] >> principalcomponent["input"],  
-         principalcomponent["rectangles"] >> rectangle2msg[:],
+         passthrough3d["output"] >> principalcomponent["input"]  
+         ]
+
+## OUTPUT
+graph += [principalcomponent["rectangles"] >> rectangle2msg[:],
          principalcomponent["eigenvectors"] >> recalibrate2msg["frames"],
          principalcomponent["centroids"] >> recalibrate2msg["origins"],
-         recalibrate2msg[:] >> poseStamped_pub[:],
+         extract_header[:] >> recalibrate2msg["header"],
+         passthrough3d["output"] >> cloud2msg_main[:],
+         colorize[:] >> cloud2msg_seg[:]
+      	 ]
+
+## PUBLISH
+graph += [recalibrate2msg["pose_stamped_msg"] >> poseStamped_pub[:],
          rectangle2msg["rectanglemsg0"] >> rectpub0[:],
          rectangle2msg["rectanglemsg1"] >> rectpub1[:],
          rectangle2msg["rectanglemsg2"] >> rectpub2[:],
          rectangle2msg["rectanglemsg3"] >> rectpub3[:],
          rectangle2msg["rectanglemsg4"] >> rectpub4[:],
          rectangle2msg["rectanglemsg5"] >> rectpub5[:],
-         #colorize[:] >> viewer["input"]
-         passthrough3d["output"] >> cloud2msg_main[:],
          cloud2msg_main[:] >> cloud_pub_main[:],
-         colorize[:] >> cloud2msg_seg[:],
-         cloud2msg_seg[:] >> cloud_pub_seg[:]
-	]
+         cloud2msg_seg[:] >> cloud_pub_seg[:],
+         recalibrate2msg["surf_name"] >> surfName_pub[:]
+         ]
+
 #graph = [cloud_sub["output"] >> msg2cloud[:],
 #         msg2cloud[:] >> cloud2msg[:],
 #         cloud2msg[:] >> cloud_pub[:]
 #	]
 
+### EXECUTION ###
 plasm = ecto.Plasm()
 plasm.connect(graph)
 
-#run_plasm(options, plasm, locals=vars())
-plasm.execute(niter=1)
+run_plasm(options, plasm, locals=vars())
+#plasm.execute(niter=1)
 
 print "plasm_ecto executed"
 
